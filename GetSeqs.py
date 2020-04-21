d440a257a48cd597570c0a4c78afe1e5fd6c4b49
Paulina Panek <39210600+Paulina-Panek@users.noreply.github.com>
2020-04-21 12:53:47 -1000

    Add files via upload

--- GetSeqs.py
+++ GetSeqs.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # Paulina Panek
 # April 2020
 # Script parsing result xml file to get name of the sequence, classification, sequence, length
@@ -23,6 +22,7 @@ def numberRecords(ListRecords):
 
 
 def Classify(ListRecords):
+    #assigns group, prints with sequence
     counter = 0
     for seq_record in SeqIO.parse(ListRecords, "gb"):
         if seq_record.annotations["taxonomy"][2] == "Ecdysozoa":  #classify as invertebrate
@@ -40,7 +40,14 @@ def Classify(ListRecords):
             else:
                 print(seq_record.annotations["source"] + ", (R)")
 
-        elif seq_record.annotations["taxonomy"][6] == "Lepidosauria": #classify as reptile
+        elif seq_record.annotations["taxonomy"][6] == "Archosauria":  # classify as bird
+            if seq_record.annotations["taxonomy"][11] == "Aves":  # bird
+                print(seq_record.annotations["source"] + ", (B)")
+            else:
+                print(seq_record.annotations["source"] + ", UNCLASSIFIED FIX ME")
+                counter = counter + 1
+
+        elif seq_record.annotations["taxonomy"][6] == "Lepidosauria" or  seq_record.annotations["taxonomy"][6] == "Testudines + Archosauria group":
             print(seq_record.annotations["source"] + ", (R)")
 
         elif seq_record.annotations["taxonomy"][6] == "Mammalia":
@@ -51,7 +58,10 @@ def Classify(ListRecords):
         else:
             print(seq_record.annotations["source"] + ", UNCLASSIFIED FIX ME")
             counter = counter + 1
-    print(counter)
+        print(seq_record.seq)
+
+
+    print("Number of unclassified species:", counter)
 
 Classify("arc_sequences_04202020.gp")
 
@@ -63,21 +73,5 @@ Classify("arc_sequences_04202020.gp")
     #print(seq_record.annotations["taxonomy"][0])
     #print(seq_record.annotations)
     #print(len(seq_record)) #length of sequence
-=======
-# Paulina Panek
-# April 2020
-# Script parsing result xml file to get name of the sequence, classification, sequence, length
-
-import Bio
-from Bio import Entrez
-Entrez.email = "ppanek@hpu.edu"
-from Bio import SeqIO
 
 
-for seq_record in SeqIO.parse("arc_sequences_04202020.gp","gb"): #uses GenPept file
-    #print(seq_record.description) #protein name [organism]
-    #print(seq_record.seq) # sequence
-    #print(seq_record.annotations["source"]) #name (common name)
-    print(seq_record.annotations["taxonomy"]) #name (common name)
-    #print(seq_record.annotations)
->>>>>>> origin/master
