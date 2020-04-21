f90381a70d32e76388035d8b1b53a1377131662f
Paulina Panek <39210600+Paulina-Panek@users.noreply.github.com>
2020-04-21 09:56:10 -1000

    Add files via upload

--- GetSeqs.py
+++ GetSeqs.py
@@ -1,3 +1,69 @@
+<<<<<<< HEAD
+# Paulina Panek
+# April 2020
+# Script parsing result xml file to get name of the sequence, classification, sequence, length
+
+import Bio
+from Bio import Entrez
+Entrez.email = "ppanek@hpu.edu"
+from Bio import SeqIO
+from io import StringIO
+from Bio import GenBank
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
+from Bio.SeqUtils.CheckSum import seguid
+
+
+def numberRecords(ListRecords):
+    #Function rints number of records
+    records = list(SeqIO.parse(ListRecords, "genbank"))
+    print("Found %i records" % len(records))
+
+#numberRecords("arc_sequences_04202020.gp")
+
+
+def Classify(ListRecords):
+    counter = 0
+    for seq_record in SeqIO.parse(ListRecords, "gb"):
+        if seq_record.annotations["taxonomy"][2] == "Ecdysozoa":  #classify as invertebrate
+            print(seq_record.annotations["source"] + ", (I)")
+
+        elif seq_record.annotations["taxonomy"][6] == "Amphibia":  # classify as amphibia
+            print(seq_record.annotations["source"] + ", (A)")
+
+        elif seq_record.annotations["taxonomy"][6] == "Actinopterygii":  # classify as fish
+            print(seq_record.annotations["source"] + ", (F)")
+
+        elif seq_record.annotations["taxonomy"][6] == "Archelosauria":  # classify as reptile or bird
+            if seq_record.annotations["taxonomy"][11] == "Coelurosauria" or seq_record.annotations["taxonomy"][11] == "Aves": #bird
+                print(seq_record.annotations["source"] + ", (B)")
+            else:
+                print(seq_record.annotations["source"] + ", (R)")
+
+        elif seq_record.annotations["taxonomy"][6] == "Lepidosauria": #classify as reptile
+            print(seq_record.annotations["source"] + ", (R)")
+
+        elif seq_record.annotations["taxonomy"][6] == "Mammalia":
+            if seq_record.annotations["taxonomy"][9] == "Primates":
+                print(seq_record.annotations["source"] + ", (P)")
+            else:
+                print(seq_record.annotations["source"] + ", (M)")
+        else:
+            print(seq_record.annotations["source"] + ", UNCLASSIFIED FIX ME")
+            counter = counter + 1
+    print(counter)
+
+Classify("arc_sequences_04202020.gp")
+
+
+#for seq_record in SeqIO.parse("arc_sequences_04202020.gp","gb"): #uses GenPept file
+    #print(seq_record.description) #protein name [organism]
+    #print(seq_record.seq) # sequence
+    #print(seq_record.annotations["source"]) #name (common name)
+    #print(seq_record.annotations["taxonomy"][0])
+    #print(seq_record.annotations)
+    #print(len(seq_record)) #length of sequence
+=======
 # Paulina Panek
 # April 2020
 # Script parsing result xml file to get name of the sequence, classification, sequence, length
@@ -14,3 +80,4 @@ for seq_record in SeqIO.parse("arc_sequences_04202020.gp","gb"): #uses GenPept f
     #print(seq_record.annotations["source"]) #name (common name)
     print(seq_record.annotations["taxonomy"]) #name (common name)
     #print(seq_record.annotations)
+>>>>>>> origin/master
